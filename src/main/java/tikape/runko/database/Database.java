754b5d3ee2a1c14d94c20573e62160de93c556f2
Aleksi95 <aleksi.laiho@helsinki.fi>
2017-06-17 14:04:07 +0300

    postgres

--- src/main/java/tikape/runko/database/Database.java
+++ src/main/java/tikape/runko/database/Database.java
@@ -1,6 +1,8 @@
 package tikape.runko.database;
 
 import java.sql.*;
+import java.util.*;
+import java.net.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,14 +12,35 @@ public class Database {
 
     public Database(String databaseAddress) throws ClassNotFoundException {
         this.databaseAddress = databaseAddress;
+
+        init();
     }
 
     public Connection getConnection() throws SQLException {
+        if (this.databaseAddress.contains("postgres")) {
+            try {
+                URI dbUri = new URI(databaseAddress);
+
+                String username = dbUri.getUserInfo().split(":")[0];
+                String password = dbUri.getUserInfo().split(":")[1];
+                String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+                return DriverManager.getConnection(dbUrl, username, password);
+            } catch (Throwable t) {
+                System.out.println("Error: " + t.getMessage());
+                t.printStackTrace();
+            }
+        }
         return DriverManager.getConnection(databaseAddress);
     }
 
     public void init() {
-        List<String> lauseet = sqliteLauseet();
+        List<String> lauseet = null;
+        if (this.databaseAddress.contains("postgres")) {
+            lauseet = postgreLauseet();
+        } else {
+            lauseet = sqliteLauseet();
+        }
 
         // "try with resources" sulkee resurssin automaattisesti lopuksi
         try (Connection conn = getConnection()) {
@@ -35,48 +58,47 @@ public class Database {
         }
     }
     
-    
-    
+    private List<String> postgreLauseet() {
+         ArrayList<String> lista = new ArrayList<>();
+         lista.add("DROP TABLE Alue");
+         lista.add("DROP TABLE Kayttaja");
+         lista.add("DROP TABLE Avaus");
+         lista.add("DROP TABLE Viesti");
+         
+         lista.add("CREATE TABLE Alue (nimi VARCHAR(20) PRIMARY KEY);");
 
-    private List<String> sqliteLauseet() {
-        ArrayList<String> lista = new ArrayList<>();
+        lista.add("CREATE TABLE Kayttaja (id INTEGER SERIAL PRIMARY KEY, nimi varchar(15));");
 
-        // tietokantataulujen luomiseen tarvittavat komennot suoritusjärjestyksessä
-        lista.add("CREATE TABLE Alue (nimi VARCHAR(20)  PRIMARY KEY);");
-        
-        lista.add("CREATE TABLE Kayttaja (id INTEGER PRIMARY KEY, nimi varchar(15));");
-        
-        lista.add("CREATE TABLE Avaus (avaus_id INTEGER PRIMARY KEY, avaus VARCHAR(100),"
+        lista.add("CREATE TABLE Avaus (avaus_id INTEGER SERIAL PRIMARY KEY, avaus VARCHAR(100),"
                 + "timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,"
                 + "alue varchar(20) NOT NULL,"
                 + "FOREIGN KEY (alue) REFERENCES Alue(nimi));");
-        
-        lista.add("CREATE TABLE Viesti(viesti_id INTEGER PRIMARY KEY, "
+
+        lista.add("CREATE TABLE Viesti(viesti_id INTEGER SERIAL PRIMARY KEY, "
                 + "kayttaja INTEGER NOT NULL, viesti VARCHAR(300),"
                 + "timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,"
                 + "alue VARCHAR(20),avaus INTEGER NOT NULL,"
                 + "FOREIGN KEY (avaus) REFERENCES Avaus(avaus_id),"
                 + "FOREIGN KEY (kayttaja) REFERENCES kayttaja(id),"
                 + "FOREIGN KEY (alue) REFERENCES alue(nimi));");
-        
+
         //Lisätään käyttäjiä:
-        
         lista.add("INSERT INTO Kayttaja(id,nimi) VALUES(1, 'Jonne')");
         lista.add("INSERT INTO Kayttaja(id,nimi) VALUES(2, 'Joni')");
         lista.add("INSERT INTO Kayttaja(id,nimi) VALUES(3, 'Jonna')");
         lista.add("INSERT INTO Kayttaja (id,nimi) VALUES(4, 'Anni')");
-        
+
         //Lisätään Alueita
         lista.add("INSERT INTO Alue(nimi) VALUES('sekalainen')");
         lista.add("INSERT INTO Alue (nimi) VALUES('kalat')");
         lista.add("INSERT INTO Alue (nimi) VALUES('koirat')");
-        
+
         // Lisätään keskustelunavauksia
         lista.add("INSERT INTO Avaus (avaus, alue)"
                 + " VALUES('Miksi', 'sekalainen')");
         lista.add("INSERT INTO Avaus (avaus, alue) VALUES('Siksi', 'sekalainen')");
         lista.add("INSERT INTO Avaus (avaus,alue) VALUES('Hauki kala','kalat')");
-        
+
         //Lisätään vastauksia
         lista.add("INSERT INTO Viesti (kayttaja, viesti,alue, avaus)"
                 + " VALUES( 4, 'Kissa on kala','kalat', 3)");
@@ -88,7 +110,59 @@ public class Database {
                 + "VALUES( 3, 'Kissa on kala','kalat', 3)");
         lista.add("INSERT INTO Viesti (kayttaja, viesti,alue, avaus)"
                 + " VALUES(4, 'Kissa on kala','sekalainen', 3)");
-        
+
+        return lista;
+    }
+
+    private List<String> sqliteLauseet() {
+        ArrayList<String> lista = new ArrayList<>();
+
+        // tietokantataulujen luomiseen tarvittavat komennot suoritusjärjestyksessä
+        lista.add("CREATE TABLE Alue (nimi VARCHAR(20)  PRIMARY KEY);");
+
+        lista.add("CREATE TABLE Kayttaja (id INTEGER PRIMARY KEY, nimi varchar(15));");
+
+        lista.add("CREATE TABLE Avaus (avaus_id INTEGER PRIMARY KEY, avaus VARCHAR(100),"
+                + "timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,"
+                + "alue varchar(20) NOT NULL,"
+                + "FOREIGN KEY (alue) REFERENCES Alue(nimi));");
+
+        lista.add("CREATE TABLE Viesti(viesti_id INTEGER PRIMARY KEY, "
+                + "kayttaja INTEGER NOT NULL, viesti VARCHAR(300),"
+                + "timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,"
+                + "alue VARCHAR(20),avaus INTEGER NOT NULL,"
+                + "FOREIGN KEY (avaus) REFERENCES Avaus(avaus_id),"
+                + "FOREIGN KEY (kayttaja) REFERENCES kayttaja(id),"
+                + "FOREIGN KEY (alue) REFERENCES alue(nimi));");
+
+        //Lisätään käyttäjiä:
+        lista.add("INSERT INTO Kayttaja(id,nimi) VALUES(1, 'Jonne')");
+        lista.add("INSERT INTO Kayttaja(id,nimi) VALUES(2, 'Joni')");
+        lista.add("INSERT INTO Kayttaja(id,nimi) VALUES(3, 'Jonna')");
+        lista.add("INSERT INTO Kayttaja (id,nimi) VALUES(4, 'Anni')");
+
+        //Lisätään Alueita
+        lista.add("INSERT INTO Alue(nimi) VALUES('sekalainen')");
+        lista.add("INSERT INTO Alue (nimi) VALUES('kalat')");
+        lista.add("INSERT INTO Alue (nimi) VALUES('koirat')");
+
+        // Lisätään keskustelunavauksia
+        lista.add("INSERT INTO Avaus (avaus, alue)"
+                + " VALUES('Miksi', 'sekalainen')");
+        lista.add("INSERT INTO Avaus (avaus, alue) VALUES('Siksi', 'sekalainen')");
+        lista.add("INSERT INTO Avaus (avaus,alue) VALUES('Hauki kala','kalat')");
+
+        //Lisätään vastauksia
+        lista.add("INSERT INTO Viesti (kayttaja, viesti,alue, avaus)"
+                + " VALUES( 4, 'Kissa on kala','kalat', 3)");
+        lista.add("INSERT INTO Viesti (kayttaja, viesti,alue, avaus)"
+                + "VALUES(1, 'Koira on kala',  'kalat', 3)");
+        lista.add("INSERT INTO Viesti (kayttaja, viesti ,alue, avaus)"
+                + "VALUES( 2, 'Ankka on kala', 'kalat', 3)");
+        lista.add("INSERT INTO Viesti (kayttaja, viesti ,alue, avaus)"
+                + "VALUES( 3, 'Kissa on kala','kalat', 3)");
+        lista.add("INSERT INTO Viesti (kayttaja, viesti,alue, avaus)"
+                + " VALUES(4, 'Kissa on kala','sekalainen', 3)");
 
         return lista;
     }
