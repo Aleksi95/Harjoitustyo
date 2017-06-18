8c530695ce4f80d02db96c0422446011306ed70e
Minna Perälampi <minna.peralampi@helsinki.fi>
2017-06-18 08:09:09 +0300

    Timestamp fix vol2

--- src/main/java/tikape/runko/database/ViestiDao.java
+++ src/main/java/tikape/runko/database/ViestiDao.java
@@ -29,10 +29,10 @@ public class ViestiDao implements Dao<Viesti, Integer> {
     public Viesti findOne(Integer key) throws SQLException {
         Connection connection = database.getConnection();
         PreparedStatement stmt = connection.prepareStatement("SELECT v.viesti, "
-                + "v.viesti_id, k.nimi as kayttaja, v.alue, v.timestamp, "
-                + "v.avaus"
-                + "FROM Viesti v LEFT JOIN Avaus ka "
-                + "ON v.avaus = ka.avaus_id"
+                + "v.viesti_id, k.nimi as kayttaja, v.alue, "
+                + "v.timestamp AT TIME ZONE 'EETDST', "
+                + "v.avausmFROM Viesti v "
+                + "LEFT JOIN Avaus ka ON v.avaus = ka.avaus_id "
                 + "LEFT JOIN Kayttaja k ON v.kayttaja = k.id "
                 + "WHERE v.avaus = ? ORDER BY v.timestamp");
 
